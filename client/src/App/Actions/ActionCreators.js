befa5899ddb4366bd693fa3c8b3f6c6cea852d51
Michal Cumpl <cumpl@siberia.local>
2017-05-28 22:13:34 +0200

    Update default bounds

--- client/src/App/Actions/ActionCreators.js
+++ client/src/App/Actions/ActionCreators.js
@@ -20,7 +20,7 @@ function receiveFeatures(features) {
   }
 }
 
-export function fetchFeatures(bounds = 'asd', zoom = 12) {
+export function fetchFeatures(bounds = {south: 4.095870312146702, west: -136.3232421875, north: 53.469301532231576, east: -83.6767578125}, zoom = 5) {
   return dispatch => {
     dispatch(requestFeatures(bounds, zoom))
 
